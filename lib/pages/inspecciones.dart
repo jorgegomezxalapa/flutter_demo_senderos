51162f5fd49554ced7bee5a247183c6758af0745
Ivan Ramos <jrgz305@gmail.com>
2023-05-26 14:06:39 -0600

    Refactorizacion de codigo de la tabla de inspeccion e integracion de font Montserrat

--- lib/pages/inspecciones.dart
+++ lib/pages/inspecciones.dart
@@ -29,203 +29,213 @@ class ModuloInspeccionState extends State<ModuloInspeccion> {
   void initState() {
     super.initState();
     _getInspecciones();
+
+  }
+
+  List<DataColumn> buildDataColumns() {
+    return const <DataColumn>[
+      DataColumn(
+        label: Expanded(
+          child: Text(
+            'Fecha de inspección',
+            style: TextStyle(
+                fontFamily: 'Montserrat',
+                fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+      DataColumn(
+        label: Expanded(
+          child: Text(
+            'Nombre o razón social',
+            style: TextStyle(
+              fontFamily: 'Montserrat',
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+      DataColumn(
+        label: Expanded(
+          child: Text(
+            'Domicilio',
+            style: TextStyle(
+              fontFamily: 'Montserrat',
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+      DataColumn(
+        label: Expanded(
+          child: Text(
+            'Subtipo de actuación',
+            style: TextStyle(
+              fontFamily: 'Montserrat',
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+      DataColumn(
+        label: Expanded(
+          child: Text(
+            'Materia',
+            style: TextStyle(
+              fontFamily: 'Montserrat',
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+      DataColumn(
+        label: Expanded(
+          child: Text(
+            'Acciones',
+            style: TextStyle(
+              fontFamily: 'Montserrat',
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    ];
+  }
+
+
+  List<DataRow> generateDataRows(List<Map<String, Object?>> inspecciones) {
+
+    return inspecciones.map((inspeccion) {
+      String inspeccionId = inspeccion[DatabaseHelper.columnInspeccionIdInspeccion].toString();
+      int inspeccion_id = int.parse(inspeccionId);
+      var fecha_inspeccion = inspeccion[DatabaseHelper.columnInFecInspeccionInspeccion].toString();
+      var nombre_razon_social = inspeccion[DatabaseHelper.columnInCtRazonSocialInspeccion].toString();
+      var domicilio = inspeccion[DatabaseHelper.columnInDomicilioInspeccionInspeccion].toString();
+      String subtipoActuacionId = inspeccion[DatabaseHelper.columnSubtipoInspeccionIdInspeccion].toString();
+      var subtipo_actuacion_id = int.parse(subtipoActuacionId);
+      var subtipo_actuacion = 'Extraordinaria';
+      switch (subtipo_actuacion_id) {
+        case 1:
+          subtipo_actuacion = 'Inicial';
+          break;
+        case 2:
+          subtipo_actuacion = 'Periodica';
+          break;
+        case 3:
+          subtipo_actuacion = 'Extraordinaria';
+          break;
+        case 4:
+          subtipo_actuacion = 'Comprobacion de medidas';
+          break;
+        case 5:
+          subtipo_actuacion = 'Extraordinaria de orientacion y asesoria';
+          break;
+        default:
+          subtipo_actuacion = '';
+      }
+      String materiaId = inspeccion[DatabaseHelper.columnMateriaIdInspeccion].toString();
+      int materia_id = int.parse(materiaId);
+      var materia = '';
+      switch (materia_id) {
+        case 1:
+          materia = 'SH';
+          break;
+        case 2:
+          materia = 'CGT';
+          break;
+        case 3:
+          materia = 'CA';
+          break;
+        case 4:
+          materia = 'CGT';
+          break;
+        case 5:
+          materia = 'SH';
+          break;
+        default:
+          materia = 'SH';
+      }
+      return DataRow(
+        cells: [
+          DataCell(Text(fecha_inspeccion)),
+          DataCell(Text(nombre_razon_social)),
+          DataCell(Text(domicilio)),
+          DataCell(Text(subtipo_actuacion)),
+          DataCell(Text(materia)),
+          DataCell(
+              Wrap(
+                children: [
+                  GestureDetector(
+                    onTap: (){
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => FormularioDatosGenerales(inspeccionId:inspeccion_id),
+                        ),
+                      );
+                    },
+                    child: const Text('Seleccionar',
+                      style: TextStyle(
+                          decoration: TextDecoration.underline
+                      ),),
+                  ),
+                  const Icon(Icons.cloud_upload_outlined),
+                ],
+              ),
+          ),
+          // Agrega más DataCell según tus necesidades
+        ],
+      );
+    }).toList();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      body: SingleChildScrollView(
+      body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    widget.titulo ?? "Formulario",
-                    style: const TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: 1.4,
-                        decoration: TextDecoration.underline),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Theme(
-                    data: Theme.of(context).copyWith(
-                      dataTableTheme:  DataTableThemeData(
-                        decoration: BoxDecoration(color: Colors.yellow.shade100), // Selecciona el color aquí
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text('Mis inspecciones',
+                      style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                          decoration: TextDecoration.underline
                       ),
                     ),
-                    child: SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: DataTable(
-                        border: TableBorder.all(width: 1),
-                        columnSpacing: 10,
-                        dataRowColor: MaterialStateProperty.resolveWith<Color>((Set<MaterialState> states) {
-                          if (states.contains(MaterialState.selected)) {
-                            return Theme.of(context).colorScheme.primary.withOpacity(0.08);
-                          }
-                          return Colors.white; // Selecciona el color aquí
+                  ],
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                FractionallySizedBox(
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: DataTableTheme(
+                      data: DataTableThemeData(
+                        headingRowColor: MaterialStateColor.resolveWith((states) {
+                          return const Color(0xFFEFEBDD); // Reemplaza el valor con tu color personalizado
                         }),
-                        columns: const <DataColumn>[
-                          DataColumn(
-                            label: Text(
-                              'Fecha de inspección',
-                              style: TextStyle(fontStyle: FontStyle.italic),
-                            ),
-                          ),
-                          DataColumn(
-                            label: Text(
-                              'Nombre o razón social',
-                              style: TextStyle(fontStyle: FontStyle.italic),
-                            ),
-                          ),
-                          DataColumn(
-                            label: Text(
-                              'Domicilio',
-                              style: TextStyle(fontStyle: FontStyle.italic),
-                            ),
-                          ),
-                          DataColumn(
-                            label: Text(
-                              'Subtipo de actuación',
-                              style: TextStyle(fontStyle: FontStyle.italic),
-                            ),
-                          ),
-                          DataColumn(
-                            label: Text(
-                              'Materia',
-                              style: TextStyle(fontStyle: FontStyle.italic),
-                            ),
-                          ),
-                          DataColumn(
-                            label: Text(
-                              'Acciones',
-                              style: TextStyle(fontStyle: FontStyle.italic),
-                            ),
-                          ),
-                        ],
-                        rows: _inspecciones.map((inspeccion) {
-                          var inspeccion_id = inspeccion[DatabaseHelper.columnInspeccionIdInspeccion];
-                          var fecha_inspeccion = inspeccion[DatabaseHelper.columnInFecInspeccionInspeccion];
-                          var nombre_razon_social = inspeccion[DatabaseHelper.columnInCtRazonSocialInspeccion];
-                          var domicilio = inspeccion[DatabaseHelper.columnInDomicilioInspeccionInspeccion];
-                          var subtipo_actuacion_id = inspeccion[DatabaseHelper.columnSubtipoInspeccionIdInspeccion];
-                          var subtipo_actuacion = 'Extraordinaria';
-                          switch (subtipo_actuacion_id) {
-                            case 1:
-                              subtipo_actuacion = 'Inicial';
-                              break;
-                            case 2:
-                              subtipo_actuacion = 'Periodica';
-                              break;
-                            case 3:
-                              subtipo_actuacion = 'Extraordinaria';
-                              break;
-                            case 4:
-                              subtipo_actuacion = 'Comprobacion de medidas';
-                              break;
-                            case 5:
-                              subtipo_actuacion = 'Extraordinaria de orientacion y asesoria';
-                              break;
-                            default:
-                              subtipo_actuacion = '';
-                          }
-                          int materia_id = inspeccion[DatabaseHelper.columnMateriaIdInspeccion];
-                          var materia = '';
-                          switch (materia_id) {
-                            case 1:
-                              materia = 'SH';
-                              break;
-                            case 2:
-                              materia = 'CGT';
-                              break;
-                            case 3:
-                              materia = 'CA';
-                              break;
-                            case 4:
-                              materia = 'CGT';
-                              break;
-                            case 5:
-                              materia = 'SH';
-                              break;
-                            default:
-                              materia = 'SH';
-                          }
-                          return DataRow(
-                            cells: <DataCell>[
-                               DataCell(
-                                 ConstrainedBox(
-                                   constraints: const BoxConstraints(maxWidth: 350),
-                                   child: Text(fecha_inspeccion, overflow: TextOverflow.ellipsis),
-                                 ),
-                               ),
-                              DataCell(
-                                  ConstrainedBox(
-                                    constraints: const BoxConstraints(maxWidth: 350),
-                                    child: Text(nombre_razon_social, overflow: TextOverflow.ellipsis),
-                                  ),
-                              ),
-                              DataCell(
-                                  ConstrainedBox(
-                                    constraints: const BoxConstraints(maxWidth: 350),
-                                    child: Text(domicilio, overflow: TextOverflow.ellipsis),
-                                  ),
-                              ),
-                              DataCell(
-                                  ConstrainedBox(
-                                    constraints: const BoxConstraints(maxWidth: 350),
-                                    child: Text(subtipo_actuacion, overflow: TextOverflow.ellipsis),
-                                  ),
-                              ),
-                              DataCell(
-                                  ConstrainedBox(
-                                    constraints: const BoxConstraints(maxWidth: 350),
-                                    child: Text(materia, overflow: TextOverflow.ellipsis),
-                                  ),
-                              ),
-                              DataCell(
-                                  Row(
-                                    children: [
-                                      Padding(
-                                        padding: const EdgeInsets.only(right: 30),
-                                        child: GestureDetector(
-                                          onTap: (){
-                                            Navigator.push(
-                                              context,
-                                              MaterialPageRoute(
-                                                builder: (context) => FormularioDatosGenerales(inspeccionId:inspeccion_id),
-                                              ),
-                                            );
-                                          },
-                                          child: const Text('Seleccionar',
-                                            style: TextStyle(
-                                                decoration: TextDecoration.underline
-                                            ),),
-                                        ),
-                                      ),
-                                      const Icon(Icons.cloud_upload_outlined),
-                                    ],
-                                  ),
-                              ),
-                            ],
-                          );
-                        }).toList(),
+                        dividerThickness: 2.0, // Ajusta el grosor del borde inferior según sea necesario
+
+                      ),
+                      child: DataTable(
+                        columns: buildDataColumns(),
+                        rows: generateDataRows(_inspecciones),
+                        
                       ),
                     ),
                   ),
-                ],
-              ),
-            ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
